d072b04f506f7bfcd8f0ccdc6ffdfac08f1cba09
yanpla <me@yanpla.nl>
2026-07-14 17:39:16 +0000

    Add Nix package derivation for website build and server
    - Add node_modules fixed-output derivation using bun install
    - Add website derivation that builds with Astro and wraps the SSR server entry
    - Expose as packages.default

--- flake.nix
+++ flake.nix
@@ -19,8 +19,80 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         vp = nix-vite-plus.packages.${system}.vp;
+        node_modules = pkgs.stdenv.mkDerivation {
+          pname = "website-node-modules";
+          version = "0.0.1";
+          src = pkgs.lib.fileset.toSource {
+            root = ./.;
+            fileset = pkgs.lib.fileset.unions [
+              ./package.json
+              ./bun.lock
+            ];
+          };
+
+          nativeBuildInputs = [ pkgs.bun ];
+
+          buildPhase = ''
+            export HOME=$TMPDIR
+            bun install --frozen-lockfile --no-progress --ignore-scripts
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp -R node_modules $out/node_modules
+          '';
+
+          dontFixup = true;
+          outputHashMode = "recursive";
+          outputHashAlgo = "sha256";
+          outputHash = "sha256-e/dRh4P7cSzJiDM9Olqeu6PH2QvDJ9grcQDKe0uEl+g=";
+        };
+
+        website = pkgs.stdenv.mkDerivation {
+          pname = "website";
+          version = "0.0.1";
+          src = pkgs.lib.fileset.toSource {
+            root = ./.;
+            fileset = pkgs.lib.fileset.difference ./. (
+              pkgs.lib.fileset.unions [
+                (pkgs.lib.fileset.maybeMissing ./node_modules)
+                (pkgs.lib.fileset.maybeMissing ./dist)
+                ./flake.nix
+                ./flake.lock
+              ]
+            );
+          };
+
+          nativeBuildInputs = [
+            pkgs.bun
+            pkgs.nodejs_24
+            pkgs.makeWrapper
+          ];
+
+          buildPhase = ''
+            export HOME=$TMPDIR
+            export ASTRO_TELEMETRY_DISABLED=1
+            cp -R ${node_modules}/node_modules node_modules
+            chmod -R u+w node_modules
+            patchShebangs node_modules
+            bun run build
+          '';
+
+          installPhase = ''
+            mkdir -p $out/lib/website $out/bin
+            cp -R dist $out/lib/website/dist
+            cp -R node_modules $out/lib/website/node_modules
+
+            makeWrapper ${pkgs.nodejs_24}/bin/node $out/bin/website \
+              --add-flags "$out/lib/website/dist/server/entry.mjs" \
+              --set-default HOST 0.0.0.0 \
+              --set-default PORT 4321
+          '';
+        };
       in
       {
+        packages.default = website;
+
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.bun
